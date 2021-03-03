e25bc426f3af40fb7edef900aaa7f3f2194747c7
Tom Mount <tmountjr@gmail.com>
2021-03-02 21:24:13 -0500

    initial commit

---
+++ src/Form/AgcdnSettingsForm.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\pantheon_agcdn_management\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class AgcdnSettingsForm.
+ */
+class AgcdnSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'pantheon_agcdn_management.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'agcdn_settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('pantheon_agcdn_management.settings');
+    $form['api_key'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('API Key'),
+      '#description' => $this->t('The API key provided by Pantheon'),
+      '#maxlength' => 64,
+      '#size' => 64,
+      '#default_value' => $config->get('api_key'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $this->config('pantheon_agcdn_management.settings')
+      ->set('api_key', $form_state->getValue('api_key'))
+      ->save();
+  }
+
+}
